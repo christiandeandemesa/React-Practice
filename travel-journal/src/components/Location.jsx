2550522145b7dcb639c32760dc46cee38fd5d4be
Christian Dean Demesa <christiandeandemesa@gmail.com>
2022-02-02 17:29:41 -0800

    Changed images and added styling

--- travel-journal/src/components/Location.jsx
+++ travel-journal/src/components/Location.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 // Imports a local image and gives it a name to be called on (marker).
-import marker from '../images/location-marker.jpg';
+import marker from '../images/location-marker.png';
+// Imports this component's stylesheet.
+import styles from '../styles/Location.module.scss';
 
 // Takes props as a parameter to get props passed into this component from App.js.
 function Location(props) {
     return (
-        <div>
+        <div id={styles.card}>
             {/* props is an object, place is an object passed into props, and img is a key in place which returns that key's value.
                 require() is used to load a relative path toward an image. */}
-            <img src={require(`../images/${props.place.img}`)}/>
-            <div>
-                <div>
+            <div id={styles.left_card}>
+                <img src={require(`../images/${props.place.img}`)} alt={'props.place.img'} id={styles.place}/>
+            </div>
+            <div id={styles.right_card}>
+                <div id={styles.header}>
                     {/* Uses the local image we imported earlier. */}
-                    <img src={marker} alt={'marker'}/>
+                    <img src={marker} alt={'marker'} id={styles.marker}/>
                     <h2>{props.place.country}</h2>
                     <h2>View on Google Maps</h2>
                 </div>
--- travel-journal/src/components/Navbar.jsx
+++ travel-journal/src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import globe from '../images/globe.png';
+import globe from '../images/globe.jpg';
+import styles from '../styles/Navbar.module.scss';
 
 function Navbar() {
     return (
-        <nav>
+        <nav id={styles.navbar}>
             <div>
-                <img src={globe} alt={'globe'}/>
+                <img src={globe} alt={'globe'} id={styles.navbar_img}/>
                 <h1>my travel journal</h1>
             </div>
         </nav>
